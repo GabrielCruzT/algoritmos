ca6f2aaaaa1924de919da76a87ff4ddd70de735f
Gabriel Cruz Tancredo <53840235+GabrielCruzT@users.noreply.github.com>
2019-08-21 15:04:13 -0300

    Criação da funçao de soma

--- teste/teste.c
+++ teste/teste.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 
+int soma(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
-    int a, b, soma;
+    int a, b;
     a=10;
     b=20;
-    soma = a+b;
-    printf("O valor da soma eh: %i \n",soma);
+    printf("O valor da soma eh: %i \n",soma(a,b));
     return 0;
 }
 
