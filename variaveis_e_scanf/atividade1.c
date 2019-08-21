1aa77b1897343da5ab70a4cb3f794a129e1aa2fc
Gabriel Cruz Tancredo <53840235+GabrielCruzT@users.noreply.github.com>
2019-08-21 14:53:05 -0300

    Atividade 1 de variáveis

---
+++ variaveis_e_scanf/atividade1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    a=10;
+    b=11.55;
+    printf("O valor de A e: %i \n",a);
+    printf("O valor de B e: %.02f \n",b);
+
+    return 0;
+}
