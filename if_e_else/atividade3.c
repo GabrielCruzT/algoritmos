8cd12c4df46aa6ca082c9214f204cba9ee39c24d
GabrielCruzT <53840235+GabrielCruzT@users.noreply.github.com>
2019-08-28 17:56:07 -0300

    correçao

--- if_e_else/atividade3.c
+++ if_e_else/atividade3.c
@@ -4,16 +4,16 @@ int main()
 {
     float nota1,nota2,nota3,media;
 
-    printf("Digite as tres notas referentes ao Trabalho de laboratório, Avaliação Semestral e Exame Final");
+    printf("Digite as tres notas referentes ao Trabalho de laboratÃ³rio, AvaliaÃ§Ã£o Semestral e Exame Final");
     scanf("%f%f%f",&nota1,&nota2,&nota3);
 
-    media=((nota1*2)+(nota2*3)+(nota3*5))/3;
+    media=((nota1*2)+(nota2*3)+(nota3*5))/10;
 
-    if (media>5)
+    if (media>=5)
     {
         printf("Voce foi aprovado!");
     }
-    else if (media>3 && media<4.9)
+    else if (media>=3 && media<=4.9)
     {
         printf("Voce esta de recuperacao!");
     }
