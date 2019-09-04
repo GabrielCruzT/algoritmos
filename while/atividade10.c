b29ed0b0836e17d15e85eadaaafadb812a9b53d8
GabrielCruzT <53840235+GabrielCruzT@users.noreply.github.com>
2019-09-04 18:53:06 -0300

    Update atividade10.c

--- while/atividade10.c
+++ while/atividade10.c
@@ -2,17 +2,31 @@
 
 int main()
 {
-   int num=1,divisor,cont,saida=0;
-
-   printf("menor numero divisivel por cada um dos numeros de 1 a 20 e:\n ");
-
-
-   while (saida==0)
-   {
-       cont++;
-       printf("O menor numero divisivel por %i e: \n",num);
-   }
+    int num=0,divisor=1,calculo;
+    int encontrado = 0;
+    int div = 20;
+    printf("menor numero divisivel por cada um dos numeros de 1 a %i e:\n ", div);
 
 
+    while ( encontrado < div )
+    {
+        num++;
+        printf("Numero atual: %i\n", num);
+
+        divisor = 1;
+        encontrado = 0;
+        while (divisor <= div)
+        {
+            calculo = num%divisor;
+            if (calculo == 0)
+                encontrado++;
+            divisor++;
+        }
+        printf("Divisores: %i\n", encontrado);
+
+
+    }
+
+    printf("Numero encontado: %i", num--);
     return 0;
 }
